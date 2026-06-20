6333b44c156311b487e1ab5b64bc580de8378336
Sahakyan1 <lilitsahakyan.mt440@polytechnic.am>
2026-06-20 10:44:19 +0400

    Create Product.java

---
+++ src/com/shop/exception/src/com/shop/model/Product.java
@@ -0,0 +1,34 @@
+package com.shop.model;
+import java.util.Objects;
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    public Product(int id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);  }
+
+    @Override
+    public String toString() {
+        return name + " (Price: $" + price + ", Available Stock: " + stock + ")";
+    }}
